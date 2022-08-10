079917b1be36e93c234c5a6dcb07932ab4d51ebc
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-10 19:20:52 -0300

    Cria classe PVP

---
+++ src/Battle/PVP.ts
@@ -0,0 +1,22 @@
+import Fighter from '../Fighter';
+import Battle from './Battle';
+
+class PVP extends Battle {
+  private _player2: Fighter;
+  constructor(player1: Fighter, player2: Fighter) {
+    super(player1);
+    this.player = player1;
+    this._player2 = player2;
+  }
+
+  fight(): number {
+    while (this.player.lifePoints > 0 && this._player2.lifePoints > 0) {
+      this.player.attack(this._player2);
+      this._player2.attack(this.player);
+    }
+
+    return super.fight();
+  }
+}
+
+export default PVP;
