b1109ed566ee0d048d6530ddb6461e60d742b0d4
Daniel Mendonça Acorroni <daniel.acorroni@hotmail.com>
2022-08-10 16:34:49 -0300

    Ateração na classe abstrata Archetype

--- src/Archetypes/Archetype.ts
+++ src/Archetypes/Archetype.ts
@@ -1,14 +1,26 @@
 import { EnergyType } from '../Energy';
 
 abstract class Archetype {
-  readonly name: string;
-  special: number;
-  cost: number;
+  readonly _name: string;
+  private _special: number;
+  private _cost: number;
 
   constructor(name: string) {
-    this.name = name;
-    this.cost = 0;
-    this.special = 0;
+    this._name = name;
+    this._cost = 0;
+    this._special = 0;
+  }
+
+  get name(): string {
+    return this._name;
+  }
+
+  get special(): number {
+    return this._special;
+  }
+
+  get cost(): number {
+    return this._cost;
   }
 
   static createdArchetypeInstances(): number {
